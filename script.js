ab0bdacca16e3a31e670e2bc296f0b6e7e7b792e
Sergey Shabelski <shabserg@gmail.com>
2022-06-27 11:47:11 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+
 function compute()
 {
-    p = document.getElementById("principal").value;
     
+    var principal = document.getElementById("principal").value;
+            if(principal <= 0){
+            alert("Please enter a positive number");
+            principal.focus();
+            return false;
+          }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+   
+    var interest = principal*years*rate/100;
+    var Amount = Number(principal) + Number(interest);
+
+    
+    var date = new Date();
+    var currentyear = date.getFullYear();
+    var lastyear = Number(currentyear) + Number(years);
+    
+  
+   document.getElementById("result").innerHTML="If you deposit <mark>" + principal + "</mark>, <br>at an interest rate of <mark>" + rate + "</mark>%, <br>You will receive an interest of <mark>" + interest + "</mark>,<br>i.e. a total amount of <mark>" + Amount + "</mark> in the year <mark>" + lastyear + "</mark>.";
 }
-        
+
+function show_value(x)
+{
+    
+    document.getElementById("rate_value").innerHTML=x;
+}
